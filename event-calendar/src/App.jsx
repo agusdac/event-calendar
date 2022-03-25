b4a282f6a9d6ac4f2adcd914cadf93c0cf2e9700
Agustin D'Acunto <agustin@streamhatchet.com>
2022-03-25 16:07:35 +0100

    initialized react app

---
+++ event-calendar/src/App.jsx
@@ -0,0 +1,22 @@
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Container fluid>
+        <Row>
+          <Col>Event</Col>
+          <Col>Event</Col>
+          <Col>Event</Col>
+          <Col>Event</Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default App
